e853a88582a6aa35b006bfc1d7a7f1fc91810f0f
Jack N <nystromjp@gmail.com>
2024-10-29 17:22:07 -0700

    switch to arrow (ipc) format over websocket

--- python-test/main.py
+++ python-test/main.py
@@ -9,37 +9,43 @@ import datetime
 import random
 
 
-def get_rows(n_rows: int = 1, total_rows: int = 10_000):
+def get_rows(total_rows: int = 10_000):
     t0 = datetime.datetime.now()
     x = 0
     for _ in range(total_rows):
-        rows = []
-        for _ in range(n_rows):
-            rows.append(
-                (
-                    math.floor((datetime.datetime.now() - t0).total_seconds() * 1000),
-                    math.sin(x / 100),
-                    x,
-                )
-            )
-            x += 1
-        yield rows
+        yield (
+            math.floor((datetime.datetime.now() - t0).total_seconds() * 1000),
+            math.sin(x / 100),
+            math.cos(x / 100),
+            math.sin(x / 100),
+            math.cos(x / 100),
+        )
+        x += 1
 
 
 async def websocket_serve(websocket: ws.WebSocketServerProtocol, path):
-    for rows in get_rows(n_rows=random.randint(1, 10)):
+    for rows in get_rows():
         print(rows)
-        ts, xs, ys = zip(*rows)
-        data = {"timestamp": ts, "x": xs, "y": ys}
+        a, b, c, d, e = zip(rows)
+        data = {
+            "timestamp": a,
+            "voltage": b,
+            "temperature": c,
+            "speed": d,
+            "blibblog": e,
+        }
         df = pl.DataFrame(data)
-        # arrow_data = df.to_arrow()
-        # with pa.ipc.new_stream(io.BytesIO(), arrow_data.schema) as writer:
-        #     writer.write_table(arrow_data)
-        # ipc_data = io.BytesIO().getvalue()
-        #
-        # await websocket.send(ipc_data)
-        await websocket.send(df.write_ndjson())
-        await asyncio.sleep(1 + random.uniform(-0.3, 0.3))
+        arrow_data = df.to_arrow()
+        byte_stream = io.BytesIO()
+        with pa.ipc.new_stream(byte_stream, arrow_data.schema) as writer:
+            writer.write_table(arrow_data)
+
+        ipc_data = byte_stream.getvalue()
+
+        await websocket.send(ipc_data)
+        # await websocket.send(df.write_ndjson())
+        await asyncio.sleep(1 + random.uniform(-0.5, 0.5))
+
 
 # Start the WebSocket server
 async def main():
@@ -49,7 +55,7 @@ async def main():
     loop.add_signal_handler(signal.SIGTERM, lambda: stop.set())
 
     async with ws.serve(websocket_serve, "0.0.0.0", 8000):
-        await stop.wait() # Run until stop "event" has been triggered
+        await stop.wait()  # Run until stop "event" has been triggered
 
 
 asyncio.run(main())

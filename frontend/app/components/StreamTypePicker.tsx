42a1752208acce4f827cdf559c3a1f0225c05e94
Vir Shah <53363059+Versus7@users.noreply.github.com>
2025-01-29 17:39:33 -0800

    Adds escape and enter key combos to speed up filtering results

--- frontend/app/components/StreamTypePicker.tsx
+++ frontend/app/components/StreamTypePicker.tsx
@@ -157,8 +157,22 @@ const StreamTypePicker: React.FC<StreamTypePickerProps> = ({
                                             d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                                     </svg>
                                     <input
+                                        onKeyDown={(e) => {
+                                            if (e.key === "Escape") {
+                                               setFilter("");
+                                            }
+                                            if (e.key === "Enter") {
+                                                const filtered = recordings.filter((r) => r.toLowerCase().includes(filter.toString()));
+                                                if (filtered.length == 1) {
+                                                    setChosenRecording(filtered[0]);
+                                                    setStreamType(StreamType.PRE_RECORDED);
+                                                    setMinimized(true);
+                                                }
+                                            }
+                                        }}
                                         className="bg-slate-800 rounded-3xl font-mono pl-4 flex-1 outline-none"
                                         type="text"
+                                        value={filter}
                                         placeholder="Filter Recordings"
                                         onChange={(e) => setFilter(e.target.value)}
                                     />
